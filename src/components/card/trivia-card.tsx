a59be9dd5e9a8c1ff60baabee7ecc07310c4d5a4
Jeremy <grlj.jeremy@gmail.com>
2023-09-25 20:58:17 +0200

    debug

--- src/components/card/trivia-card.tsx
+++ src/components/card/trivia-card.tsx
@@ -1,10 +1,10 @@
 import {
-  Box,
-  Button,
-  Card,
-  CardContent,
-  TextField,
-  Typography,
+	Box,
+	Button,
+	Card,
+	CardContent,
+	TextField,
+	Typography,
 } from "@mui/material";
 import TriviaSelect from "../common/trivia-select";
 import { useEffect, useState } from "react";
@@ -12,69 +12,69 @@ import { apiClient } from "../../constants/api-client";
 import { Globals } from "../../utils/utils";
 
 const TriviaCard: React.FC = () => {
-  const [categories, setCategories] = useState<[]>([]);
+	const [categories, setCategories] = useState<[]>([]);
 
-  console.log(console.log(import.meta.env.MODE));
-  console.log(console.log(import.meta.env.TEST));
+	console.log(console.log(import.meta.env.MODE));
+	console.log(console.log(import.meta.env.TEST));
 
-  useEffect(() => {
-    const getCategories = async () => {
-      try {
-        const response = await apiClient.get("/api_category.php");
-        if (response.status === 200) {
-          setCategories(response.data.trivia_categories);
-        }
-      } catch (error) {
-        console.log(error);
-      }
-    };
-    getCategories();
-  }, []);
+	useEffect(() => {
+		const getCategories = async () => {
+			try {
+				const response = await apiClient.get("/api_category.php");
+				if (response.status === 200) {
+					setCategories(response.data.trivia_categories);
+				}
+			} catch (error) {
+				console.log(error);
+			}
+		};
+		getCategories();
+	}, []);
 
-  return (
-    <Card sx={{ width: "600px" }}>
-      <CardContent
-        sx={{
-          display: "flex",
-          flexDirection: "column",
-          gap: "24px",
-        }}
-      >
-        <Typography>Pick out a combination to generate trivia cards</Typography>
-        <Box
-          component="form"
-          sx={{
-            display: "flex",
-            flexDirection: "column",
-            alignItems: "center",
-            gap: "24px",
-          }}
-        >
-          <TextField
-            id="number-of-questions"
-            label="Number"
-            type="number"
-            fullWidth
-          />
-          <TriviaSelect id="category" label="Category" options={categories} />
-          <TriviaSelect
-            id="difficulty"
-            label="Difficulty"
-            options={Globals.difficulties}
-          />
-          <TriviaSelect id="type" label="Type" options={Globals.types} />
-          <Button
-            variant="contained"
-            fullWidth
-            size="large"
-            sx={{ fontSize: "1rem" }}
-          >
-            Generate trivia cards
-          </Button>
-        </Box>
-      </CardContent>
-    </Card>
-  );
+	return (
+		<Card sx={{ width: "600px" }}>
+			<CardContent
+				sx={{
+					display: "flex",
+					flexDirection: "column",
+					gap: "24px",
+				}}
+			>
+				<Typography>Pick out a combination to generate trivia cards</Typography>
+				<Box
+					component="form"
+					sx={{
+						display: "flex",
+						flexDirection: "column",
+						alignItems: "center",
+						gap: "24px",
+					}}
+				>
+					<TextField
+						id="number-of-questions"
+						label="Number"
+						type="number"
+						fullWidth
+					/>
+					<TriviaSelect id="category" label="Category" options={categories} />
+					<TriviaSelect
+						id="difficulty"
+						label="Difficulty"
+						options={Globals.difficulties}
+					/>
+					<TriviaSelect id="type" label="Type" options={Globals.types} />
+					<Button
+						variant="contained"
+						fullWidth
+						size="large"
+						sx={{ fontSize: "1rem" }}
+					>
+						Generate trivia cards
+					</Button>
+				</Box>
+			</CardContent>
+		</Card>
+	);
 };
 
 export default TriviaCard;
