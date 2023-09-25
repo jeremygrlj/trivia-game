cdfadcd9daa77cab2c0ec74c6201a769ab91a9a2
Jeremy Grlj <jeremygrlj@users.noreply.github.com>
2023-09-25 18:45:04 +0000

    Update 3 files

--- src/constants/api-client.ts
+++ src/constants/api-client.ts
@@ -1,5 +1,5 @@
 import axios from "axios";
 
 export const apiClient = axios.create({
-  baseURL: import.meta.env.API_BASE_URL || "https://opentdb.com"
+  baseURL: import.meta.env.API_BASE_URL,
 });
